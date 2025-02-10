0c4ea35845e139a06294a96b98ebe7164202feac
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-11 00:23:19 +0100

    add Menu

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,24 @@
+export const Menu = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">#home</a>
+                    </li>
+                    <li>
+                        <a href="">#works</a>
+                    </li>
+                    <li>
+                        <a href="">#about-me</a>
+                    </li>
+                    <li>
+                        <a href="">#contacts</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Menu;
