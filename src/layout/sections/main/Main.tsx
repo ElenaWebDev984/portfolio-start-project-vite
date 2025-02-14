b46cccbc3357c1cd8868236df6c6046879a0c244
Kirena <elena.job.984@gmail.com>
2025-02-14 18:49:46 +0100

    add components

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,24 +1,41 @@
 import styled from 'styled-components';
 import photo from '../../../assets/images/foto1.png';
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 
 export const Main = () => {
     return (
-        <div>
-            <div>
-                <span>Elias is a web designer and</span>
-                <h1>front-end developer</h1>
-                <p>He crafts responsive websites where technologies meet creativity</p>
-                <span>Contact me!!</span>
-            </div>
+        <StyledMain>
+            <FlexWrapper alignItems={"center"} justifyContent={'space-around'}>
+                <div>
+                    <span>Hi there!</span>
+                    <Name>I'm Elena</Name>
+                    <MainTitle>a front-end developer</MainTitle>
+                    <p>I craft responsive websites where technologies meet creativity</p>
+                    <span>Contact me!!</span>
+                </div>
 
-            <Photo src={photo} alt="foto"/>
-        </div>
+                <Photo src={photo} alt="foto"/>
+            </FlexWrapper>
+        </StyledMain>
     );
 };
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: #727578;
+`
 
 
 const Photo = styled.img`
     width: 457px;
     height: 386px;
     object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+    
+`
+
+const Name = styled.h2`
+
 `
